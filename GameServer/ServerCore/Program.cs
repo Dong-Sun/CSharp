56c5e2f7cc922591c0ac73bf33c9b4b0351774b4
DongSun <duaehdtjs20@gmail.com>
2024-03-24 20:16:03 +0900

    캐시이론

--- GameServer/ServerCore/Program.cs
+++ GameServer/ServerCore/Program.cs
@@ -6,35 +6,25 @@ namespace ServerCore
 {
     class Program
     {
-        private volatile static bool _stop = false;
-
-        private static void ThreadMain()
-        {
-            Console.WriteLine("쓰레드 시작!");
-
-            while (_stop == false)
-            {
-                // 누군가가 stop 신호를 해주기를 기다린다.
-            }
-
-            Console.WriteLine("쓰레드 종료!");
-        }
-        
         private static void Main(string[] args)
         {
-            Task t = new Task(ThreadMain);
-            t.Start();
-            
-            Thread.Sleep(1000);
-
-            _stop = true;
-
-            Console.WriteLine("Stop 호출");
-            Console.WriteLine("종료 대기중");
-            
-            t.Wait();
-            
-            Console.WriteLine("종료 성공");
+            int[,] arr = new int[10000, 10000];
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[y, x] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y, x)순서 걸린 시간 {end - now}");
+            }
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                    for (int x = 0; x < 10000; x++)
+                        arr[x, y] = 1;
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y, x)순서 걸린 시간 {end - now}");
+            }
         }
     }
 }
--- GameServer/ServerCore/obj/Release/net8.0/ServerCore.AssemblyInfo.cs
+++ GameServer/ServerCore/obj/Release/net8.0/ServerCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e57b2ca63fffbba0c12dc1e763d33b9b53641bd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3009cc85d9f704df6fd71e946dd2a8e3537dcb91")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
