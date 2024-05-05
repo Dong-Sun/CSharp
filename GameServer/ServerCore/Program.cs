20f383497cfc802a4a046d08832625ffb1ab4770
DongSun <duaehdtjs20@gmail.com>
2024-05-05 19:00:49 +0900

    Lock 기초

--- GameServer/ServerCore/Program.cs
+++ GameServer/ServerCore/Program.cs
@@ -4,20 +4,27 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // A) 같은 자원을 두개 이상의 스레드가 동시에 접근하면 여러 문제가 발생할 수 있음
-    // B) 문제들을 막기 위해서는 연산에 원자성과 순서를 보장해 주어야 함
-    // C) Interlocked 키워드를 통해 정수형 연산은 위 규칙을 지킬 수 있음
-    // D) 중간 결과 값을 보고 싶을 때는 새로운 라인에서 변수를 출력하는게 아닌
-    //    반환값을 받아서 출력해야함 (한줄 사이에 값이 바뀔 수 있음)
+    // A) Monitor 클래스의 Enter와 Exit를 사용해 구문끼리 묶어서 잠굴 수 있다
+    // B) Enter와 Exit 쌍을 맞춰주지 않으면 끝없이 기다리면서 멈춘다(데드락)
+    // C) lock 키워드를 통해 조금이나마 편리하게 관리 가능함(자동으로 잠금, 해제)
+    // D) obj == key
     class Program
     {
         private static int number = 0;
+        private static object _obj = new object();
         static void Thread_1()
         {
             for (int i = 0; i < 100000; i++)
             {
-                // All or Nothing
-                int afterValue = Interlocked.Increment(ref number);
+                lock (_obj)
+                {
+                    number++;
+                }
+                
+                // 상호배제 Mutual Exclusive
+                // Monitor.Enter(_obj);    // 문을 잠구는 행위
+                // number++;
+                // Monitor.Exit(_obj);     // 잠금을 풀어준다
             }
         }
         
@@ -25,8 +32,14 @@ namespace ServerCore
         {
             for (int i = 0; i < 100000; i++)
             {
-                // All or Nothing
-                int afterValue = Interlocked.Decrement(ref number);
+                lock (_obj)
+                {
+                    number--;
+                }
+                
+                // Monitor.Enter(_obj);
+                // number--;
+                // Monitor.Exit(_obj);
             }
         }
         
