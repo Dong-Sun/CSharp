ee39c36a90538514f6496983e89e08da851b9c72
DongSun <duaehdtjs20@gmail.com>
2024-05-10 03:57:18 +0900

    Dead Lock

--- GameServer/ServerCore/Program.cs
+++ GameServer/ServerCore/Program.cs
@@ -4,42 +4,65 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
-    // A) Monitor 클래스의 Enter와 Exit를 사용해 구문끼리 묶어서 잠굴 수 있다
-    // B) Enter와 Exit 쌍을 맞춰주지 않으면 끝없이 기다리면서 멈춘다(데드락)
-    // C) lock 키워드를 통해 조금이나마 편리하게 관리 가능함(자동으로 잠금, 해제)
-    // D) obj == key
+	class SessionManager
+	{
+		static object _lock = new object();
+
+		public static void TestSession()
+		{
+			lock (_lock)
+			{
+				
+			}
+		}
+
+		public static void Test()
+		{
+			lock (_lock)
+			{
+				UserManager.TestUser();
+			}
+		}
+	}
+	class UserManager
+	{
+		static object _lock = new object();
+
+		public static void Test()
+		{
+			lock (_lock)
+			{
+				SessionManager.TestSession();
+			}
+		}
+
+		public static void TestUser()
+		{
+			lock (_lock)
+			{
+				
+			}
+		}
+
+	}
     class Program
     {
         private static int number = 0;
         private static object _obj = new object();
+
         static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock (_obj)
-                {
-                    number++;
-                }
-                
-                // 상호배제 Mutual Exclusive
-                // Monitor.Enter(_obj);    // 문을 잠구는 행위
-                // number++;
-                // Monitor.Exit(_obj);     // 잠금을 풀어준다
+				SessionManager.Test();
             }
         }
         
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 10000; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
-                
-                // Monitor.Enter(_obj);
-                // number--;
-                // Monitor.Exit(_obj);
+				UserManager.Test();
             }
         }
         
@@ -48,6 +71,7 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+			Thread.Sleep(100);
             t2.Start();
             
             Task.WaitAll(t1, t2);
